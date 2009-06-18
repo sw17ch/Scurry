289dbce7d77b7d66ddd62a388c10c7cf809b241c
John Van Enk <vanenkj@gmail.com>
2009-06-18 00:13:10 -0400

    Added Data.Scurry.Message.
    Fleshed out a few of the data types. Pulled them into Main just to
    make sure things compile. They aren't used yet.

--- src/Data/Scurry.hs
+++ src/Data/Scurry.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+module Data.Scurry (
+    Scurry(..),
+) where
 
 import Data.Data
-
-module Data.Scurry where
 
 data Scurry = Scurry
     deriving (Data,Typeable,Show)
--- src/scurry.hs
+++ src/scurry.hs
@@ -1,4 +1,7 @@
 module Main where
 
+import Data.Scurry
+import Data.Scurry.Message
+
 main :: IO ()
 main = putStrLn "Scurry 0.1.0"
