4ef6c4f553017aea26ea7d54dc000c9faf85f554
John Van Enk <john@dorner-john-linux.(none)>
2009-07-31 13:10:56 -0400

    Added new bindings for linux TAP device. There's a test interface in Scurry.TAP as well.

--- src/C/help-linux.c
+++ src/C/help-linux.c
@@ -1,161 +1,174 @@
-#include <arpa/inet.h>
-#include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
+
+#include <netinet/ip.h>
+
+#include <sys/ioctl.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
-#include <net/route.h>
-#include <netinet/if_ether.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 #include "help.h"
 
-int open_tap(ip4_addr_t local_ip, ip4_addr_t local_mask, struct tap_info * ti);
-int get_mac(struct ifreq * ifr, int sock, struct tap_info * ti);
-void close_tap(union tap_desc * td);
+#define CLEAR(x) \
+    memset((x), 0, sizeof(*(x)))
 
-static int set_ip(struct ifreq * ifr, int sock, ip4_addr_t ip4);
-static int set_mask(struct ifreq * ifr, int sock, ip4_addr_t ip4);
-static int set_mtu(struct ifreq * ifr, int sock, unsigned int mtu);
-
-
-int open_tap(ip4_addr_t local_ip, ip4_addr_t local_mask, struct tap_info * ti)
+struct tap_desc
 {
-    struct ifreq ifr_tap;
-    int r = 0;
+    uint32_t     desc;
+    int32_t      sock;
+    struct ifreq ifr;
+};
 
-    int fd = -1;
-    int sock = -1;
-
-    if ((fd = open("/dev/net/tun", O_RDWR)) < 0)
-        return -1;
-
-    memset(&ifr_tap, 0, sizeof(ifr_tap));
-
-    /* setup tap */
-    ifr_tap.ifr_flags = IFF_TAP | IFF_NO_PI;
-
-    if ((ioctl(fd, TUNSETIFF, (void *)&ifr_tap)) < 0)
-        return -2;
-    
-    /* setup ip */
-    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-        return -3;
-
-    if (set_ip(&ifr_tap, sock, local_ip) < 0)
-        return -4;
-
-    if (set_mask(&ifr_tap, sock, local_mask) < 0)
-        return -5;
-
-    if ( ioctl(sock, SIOCGIFFLAGS, &ifr_tap) < 0)
-        return -6;
-
-    if ( get_mac(&ifr_tap,sock,ti) < 0)
-        return -7;
-
-    ifr_tap.ifr_flags |= IFF_UP;
-    ifr_tap.ifr_flags |= IFF_RUNNING;
-
-    if (ioctl(sock, SIOCSIFFLAGS, &ifr_tap) < 0)
-        return -8;
-
-    if (set_mtu(&ifr_tap, sock, 1200) < 0)
-        return -9;
-
-    ti->desc->desc = fd;
-
-    return fd;
+tap_desc_t * init_tap()
+{
+    tap_desc_t * td = malloc(sizeof(tap_desc_t));
+    CLEAR(td);
+    return td;
 }
 
-static int set_ip(struct ifreq * ifr, int sock, ip4_addr_t ip4)
+void finish_tap(tap_desc_t * td)
 {
-    struct sockaddr_in addr;
-
-    /* set the IP of this end point of tunnel */
-    memset( &addr, 0, sizeof(addr) );
-    addr.sin_addr.s_addr = ip4; /*network byte order*/
-    addr.sin_family = AF_INET;
-    memcpy( &ifr->ifr_addr, &addr, sizeof(struct sockaddr) );
-
-    if ( ioctl(sock, SIOCSIFADDR, ifr) < 0) {
-        printf("SIOCSIFADDR: %s\n", strerror(errno));
-        return -1;
-    }
-
-    return 0; 
+    free(td);
 }
 
-static int set_mask(struct ifreq * ifr, int sock, ip4_addr_t ip4)
+int32_t open_tap(tap_desc_t * td, int8_t * name)
 {
-    struct sockaddr_in addr;
+    const int8_t dev[] = "/dev/net/tun";
 
-    memset( &addr, 0, sizeof(addr) );
-    addr.sin_addr.s_addr = ip4; /*network byte order*/
-    addr.sin_family = AF_INET;
-    memcpy( &ifr->ifr_addr, &addr, sizeof(struct sockaddr) );
-    
-    if ( ioctl(sock, SIOCSIFNETMASK, ifr) < 0) {
-        printf("SIOCSIFNETMASK: %s\n", strerror(errno));
-        return -1;
-    }
-
-    return 0;
-}
-
-static int set_mtu(struct ifreq * ifr, int sock, unsigned int mtu)
-{
-    /* Set the MTU of the tap interface */
-    ifr->ifr_mtu = mtu; 
-    if (ioctl(sock, SIOCSIFMTU, ifr) < 0)  {
-        printf("SIOCSIFMTU: %s\n", strerror(errno));
-        return -1;
-    }
-
-    return 0;
-}
-
-void close_tap(union tap_desc * td)
-{
-    if (0 <= td->desc) 
+    if (0 == td)
     {
-        close(td->desc);
+        return -100;
     }
-}
+    else if (0 == name)
+    {
+        return -101;
+    }
 
-int get_mac(struct ifreq * ifr, int sock, struct tap_info * ti)
-{
-    
-    if ( ioctl(sock, SIOCGIFHWADDR, ifr) < 0) {
-        printf("SIOCGIFHWADDR: %s\n", strerror(errno));
+    int32_t fd = open(dev, O_RDWR);
+
+    if (fd < 0)
+    {
+        fprintf(stderr,"open(%s): %s\n", name, strerror(errno));
         return -1;
     }
     else
     {
-        memcpy(&(ti->mac),&(ifr->ifr_hwaddr.sa_data),6);
+        td->desc = fd;
     }
 
+    CLEAR(&(td->ifr));
+
+    td->sock = socket(AF_INET, SOCK_DGRAM, 0);
+    td->ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+
+    if (*name)
+        strncpy(td->ifr.ifr_name, name, IFNAMSIZ);
+
+    if (ioctl(td->desc, TUNSETIFF, (void *)&td->ifr) < 0)
+    {
+        fprintf(stderr,"TUNSETIFF: %s\n", strerror(errno));
+        return -2;
+    }
 
     return 0;
 }
 
-/* I HATE WINDOWS IT SUCKS SO HARD AHHH!! */
-int read_tap(union tap_desc * td, char * buf, int len)
+int32_t close_tap(tap_desc_t * td)
 {
-    int ret = read(td->desc,buf,len);
-    return ret;
+    if (0 <= td->desc)
+    {
+        close(td->desc);
+    }
+
+    return 0;
 }
 
-int write_tap(union tap_desc * td, const char * buf, int len)
+int32_t bring_up_tap(tap_desc_t * td)
 {
-    int ret = write(td->desc,buf,len);
-    return ret;
+    td->ifr.ifr_flags |= (IFF_UP | IFF_RUNNING);
+
+    if (ioctl(td->sock, SIOCSIFFLAGS, &td->ifr) < 0)
+    {
+        fprintf(stderr,"SIOCSIFFLAGS: %s\n", strerror(errno));
+        return -3;
+    }
+
+    return 0;
+}
+
+int32_t set_mtu(tap_desc_t * td, uint32_t mtu)
+{
+    td->ifr.ifr_mtu = mtu;
+
+    if (ioctl(td->sock, SIOCSIFMTU, &td->ifr) < 0)
+    {
+        fprintf(stderr,"SIOCSIFMTU: %s\n", strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+int32_t set_ip(tap_desc_t * td, uint32_t ip)
+{
+    struct sockaddr_in addr;
+    CLEAR(&addr);
+
+    addr.sin_addr.s_addr = ip;
+    addr.sin_family = AF_INET;
+    memcpy(&td->ifr.ifr_addr, &addr, sizeof(addr));
+
+    if (ioctl(td->sock, SIOCSIFADDR, &td->ifr) < 0)
+    {
+        fprintf(stderr, "SIOCSIFADDR: %s\n", strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+int32_t set_mask(tap_desc_t * td, uint32_t mask)
+{
+    struct sockaddr_in addr;
+    CLEAR(&addr);
+
+    addr.sin_addr.s_addr = mask;
+    addr.sin_family = AF_INET;
+    memcpy(&td->ifr.ifr_addr, &addr, sizeof(addr));
+
+    if ( ioctl(td->sock, SIOCSIFNETMASK, &td->ifr) < 0)
+    {
+        fprintf(stderr,"SIOCSIFNETMASK: %s\n", strerror(errno));
+        return -1;
+    }
+}
+
+int32_t get_mac(tap_desc_t * td, MACAddr mac)
+{
+    if (ioctl(td->sock, SIOCGIFHWADDR, &td->ifr) < 0)
+    {
+        fprintf(stderr,"SIOCGIFHWADDR: %s\n", strerror(errno));
+        return -1;
+    }
+    else
+    {
+        memcpy(mac,&td->ifr.ifr_hwaddr.sa_data, sizeof(MACAddr));
+    }
+
+    return 0;
+}
+
+int32_t read_tap(tap_desc_t * td, int8_t * buf, int32_t len)
+{
+    return read(td->desc,buf,len);
+}
+
+int32_t write_tap(tap_desc_t * td, const int8_t * buf, int32_t len)
+{
+    return write(td->desc,buf,len);
 }
