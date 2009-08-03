26213a6426189fe3a64d087b4ae9d47cf6b1bd04
John Van Enk <john@dorner-john-linux.(none)>
2009-08-03 15:10:14 -0400

    Fixed type signature

--- src/Scurry/TAPSrc.hs
+++ src/Scurry/TAPSrc.hs
@@ -5,7 +5,9 @@ module Scurry.TAPSrc (
 import Control.Concurrent.MVar
 
 import Scurry.FFI
+import Scurry.Scurry
 
+readTAPTask :: MVar Scurry -> MVar () -> IO ()
 readTAPTask m c = do
     test
     putMVar c ()
