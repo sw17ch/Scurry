a6f64f86fa149ebb17b58c6f03a4772b9264d7cd
John Van Enk <vanenkj@gmail.com>
2009-08-04 00:00:33 -0400

    Added IPv4 module

--- src/Scurry/Data/Packet.hs
+++ src/Scurry/Data/Packet.hs
@@ -1,6 +1,8 @@
 
 module Scurry.Data.Packet (
     module Scurry.Data.Packet.Ethernet,
+    module Scurry.Data.Packet.IPv4,
 ) where
 
 import Scurry.Data.Packet.Ethernet
+import Scurry.Data.Packet.IPv4
