4d263d236d71b3b7a20c36f9c9335ee1ea25b7b1
John Van Enk <vanenkj@gmail.com>
2009-08-19 12:23:29 -0400

    Switched to Binary from Storable
    The Storable instances are still in place but I've changed the
    structure so that Scurry.FFI gives us ByteStrings rather than
    ForeignPtrs. We're also going to be depending on Binary for our
    encoding and decoding of packets.

--- src/Scurry/Data/Packet/IPv4.hs
+++ src/Scurry/Data/Packet/IPv4.hs
@@ -18,13 +18,14 @@ module Scurry.Data.Packet.IPv4 (
     IPv4_Options(..),
 ) where
 
-import Data.Word
 import Data.Bits
-import Foreign.ForeignPtr
+import Data.Binary
+import Data.Binary.Get
+import Data.Binary.Put
+
 import Foreign.Storable
 import Foreign.Ptr
 
-import Scurry.Data.Packet.Ethernet
 import Scurry.Network.Util
 
 data IPv4 = IPv4 {
@@ -56,6 +57,66 @@ newtype IPv4_HdrChck  = IPv4_HdrChck  { unHdrChck  :: Word16 } deriving (Show)
 newtype IPv4_SrcAddr  = IPv4_SrcAddr  { unSrcAddr  :: Word32 } deriving (Show)
 newtype IPv4_DstAddr  = IPv4_DstAddr  { unDstAddr  :: Word32 } deriving (Show)
 newtype IPv4_Options  = IPv4_Options  { unOptions  :: Word32 } deriving (Show)
+
+instance Binary IPv4 where
+    get = do
+        verhdr <- getWord8
+        dif    <- getWord8
+        tot    <- getWord16be
+        ide    <- getWord16be
+        flafra <- getWord16be
+        tto    <- getWord8
+        pro    <- getWord8
+        hdc    <- getWord16be
+        sra    <- getWord32be
+        dsa    <- getWord32be
+        let (ver,hdr) = splt_verhdr verhdr
+        let (fla,fra) = splt_flafra flafra
+        opt    <- case (4 * hdr) of
+                       24 -> getWord32be >>=
+                             (return . Just . IPv4_Options)
+                       _  -> return Nothing
+        return $ IPv4 { version  = IPv4_Version  ver,
+                        hdrLen   = IPv4_HdrLen   hdr,
+                        diffServ = IPv4_DiffServ dif,
+                        totalLen = IPv4_TotalLen tot,
+                        ident    = IPv4_Ident    ide,
+                        flags    = IPv4_Flags    fla,
+                        fragOff  = IPv4_FragOff  fra,
+                        ttl      = IPv4_TTL      tto,
+                        protocol = IPv4_Protocol pro,
+                        hdrChck  = IPv4_HdrChck  hdc,
+                        srcAddr  = IPv4_SrcAddr  sra,
+                        dstAddr  = IPv4_DstAddr  dsa,
+                        options  = opt }
+        where splt_verhdr verhdr = let v = 0xF0 .&. verhdr
+                                       h = 0x0F .&. verhdr
+                                   in  (shiftR v 4, h)
+              splt_flafra flafra = let fl = 0xE000 .&. flafra
+                                       fr = 0x1FFF .&. flafra
+                                   in (fromIntegral $ shiftR fl 13, fr)
+    put i = do
+        putWord8    $ mk_verhdr (unVersion . version $ i) (unHdrLen . hdrLen $ i)
+        putWord8    $ unDiffServ . diffServ $ i
+        putWord16be $ unTotalLen . totalLen $ i
+        putWord16be $ unIdent . ident $ i
+        putWord16be $ mk_flafra (unFlags . flags $ i) (unFragOff . fragOff $ i)
+        putWord8    $ unTTL . ttl $ i
+        putWord8    $ unProtocol . protocol $ i
+        putWord16be $ unHdrChck . hdrChck $ i
+        putWord32be $ unSrcAddr . srcAddr $ i
+        putWord32be $ unDstAddr . dstAddr $ i
+        case options i of
+            (Just o) -> putWord32be $ unOptions o
+            Nothing  -> return ()
+        where
+            mk_verhdr ver hdr = let v = (0x0F .&. ver)
+                                    h = (0x0F .&. hdr)
+                                in (shiftL v 4) .|. h
+            mk_flafra fla fra = let l = (0x07   .&. fla)
+                                    r = (0x1FFF .&. fra)
+                                in (shiftL (fromIntegral l) 13) .|. r
+        
 
 instance Storable IPv4 where
     sizeOf _    = (6 * 32) -- With Options field
@@ -102,20 +163,4 @@ instance Storable IPv4 where
               splt_flafra flafra = let fl = 0xE000 .&. flafra
                                        fr = 0x1FFF .&. flafra
                                    in (fromIntegral $ shiftR fl 13, fr)
-
-instance Framed IPv4 where
-    unframe p = do
-        ehdr <- unframe p
-        case ehdr of
-            Nothing  -> return Nothing
-            (Just e) -> case etype e of
-                             ET_IP -> gptr (unpacket p) >>=
-                                      dopeek            >>=
-                                      (return . Just)
-                             _     -> return Nothing
-        where
-            -- get the correct pointer
-            seth = sizeOf (undefined :: Ethernet)
-            gptr p' = withForeignPtr p' $ \p'' -> return $ p'' `plusPtr` seth
-            dopeek = (peek . castPtr)
 
