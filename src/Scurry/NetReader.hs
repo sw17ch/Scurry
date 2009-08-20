d8deb3fbc4e6e1c68a902d292913762019799512
John Van Enk <vanenkj@gmail.com>
2009-08-20 11:13:58 -0400

    Stubbed netTask

--- src/Scurry/NetReader.hs
+++ src/Scurry/NetReader.hs
@@ -1,11 +1,19 @@
 module Scurry.NetReader (
+    NetMsg(..),
 ) where
 
-import Scurry.Crypto
+import Control.Concurrent.MVar
+import Control.Monad
 import Data.ByteString.Lazy
+
+import Scurry.Crypto
+import Scurry.Scurry
 
 data NetMsg = Reject
             | Join   { key :: ScurryPubKey }
             | Accept { key :: ScurryPubKey }
             | EncMsg { msg :: ByteString }
     deriving (Show)
+
+netTask :: MVar Scurry -> IO ()
+netTask s = forever $ return ()
