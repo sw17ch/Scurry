16b3c3a9453097e08b970af7c7d48d4d0da2ae11
John Van Enk <vanenkj@gmail.com>
2010-04-08 02:05:17 -0400

    Clicking on 'Shutdown Scurry' will... you guessed it.

--- ui/script.js
+++ ui/script.js
@@ -1,4 +1,41 @@
+function dialogMessage(msg) {
+    $('#dialog').dialog({
+        modal: true,
+        buttons: {
+            Ok: function () {
+                $(this).dialog('close');
+            }
+        }
+    }).html('<p>' + msg + '<p>');
+}
+
+/* Sends the shutdown command to Scurry. This will kill the process. */
+function sendShutdown() {
+    var button = $('#button-shutdown'),
+        txid = $.uuid();
+
+    $.ajax({
+        data: { 'txid': txid, 'event': 'shutdown' },
+        success: function (data) {
+            if (data && "OK" == data.responseCode) {
+                dialogMessage("Success!");
+            } else {
+                dialogMessage("Failure!");
+            }
+        }
+    });
+}
+
+/* Setup binds */
+function setupBinds() {
+    $('#button-shutdown').click(sendShutdown);
+}
+
 /* Kick things off plz */
 function startup() {
-    
+    $.ajaxSetup( {
+        url: 'sq',
+        dataType: 'json'
+    });
+    setupBinds();
 }
